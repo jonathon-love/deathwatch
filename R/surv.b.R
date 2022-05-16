cbf1341570a2b706cf3e84d4e11f9a837740c97a
Jonathon Love <jon@thon.cc>
2022-05-16 19:29:42 +1000

    Fixes

--- R/surv.b.R
+++ R/surv.b.R
@@ -7,10 +7,10 @@ survClass <- R6::R6Class(
     inherit = survBase,
     private = list(
         .init = function() {
-            
+
             groups <- private$.groups()
-            
-            
+
+
             ####################################
             ### Add levels dinamically to first
             ### column in Table 1
@@ -19,7 +19,7 @@ survClass <- R6::R6Class(
             summary <- self$results$summary
             for (group in groups)
                 summary$addRow(rowKey=group, list(group=group))
-            
+
             ####################################
             ### Add levels dinamically to first
             ### column in Table 3
@@ -28,7 +28,7 @@ survClass <- R6::R6Class(
             summary <- self$results$medianestimates
             for (group in groups)
                 summary$addRow(rowKey=group, list(group=group))
-            
+
             tests <- self$results$tests
             if (length(groups) >= 2) {
                 tests$addRow(rowKey=1)
@@ -36,11 +36,11 @@ survClass <- R6::R6Class(
             else {
                 tests$setVisible(FALSE)
             }
-            
-            
+
+
             ####################################
             ### Add levels dinamically to first
-            ### column in Table with pairwise 
+            ### column in Table with pairwise
             ### comparisons
             ####################################
             testspw <- self$results$testspw
@@ -54,41 +54,41 @@ survClass <- R6::R6Class(
             else {
                 testspw$setVisible(FALSE)
             }
-            
-            
+
+
             ################################
             ### Change the size of the
-            ### survival plot if 
+            ### survival plot if
             ### risk table option is ticked
             ################################
             if (self$options$risk)
-                self$results$sc$setSize(600, 600)  
-            
+                self$results$sc$setSize(600, 600)
+
         },
         .groups = function() {
             if (is.null(self$options$groups))
                 group <- NULL
             else
                 group <- self$data[[self$options$groups]]
-            
+
             groups <- levels(group)
             if (length(groups) == 0)
                 groups = ''
-            
+
             groups
         },
         .run = function() {
-            
+
             eventVarName <- self$options$event
             elapsedVarName <- self$options$elapsed
 
             if (is.null(eventVarName) || is.null(elapsedVarName))
                 return()
-            
+
             if (nrow(self$data) == 0)
                 stop('Data contains no (complete) rows')
-            
-            
+
+
             ###########################
             ### Creates the data frame
             ###########################
@@ -102,7 +102,7 @@ survClass <- R6::R6Class(
                 tmpDat <- data.frame(times = as.numeric(self$data[[elapsedVarName]]),
                                      status = ifelse(self$data[[eventVarName]] == self$options$eventLevel, 1, 0))
             }
-            
+
             ##########################
             ### Add grouping variable
             ##########################
@@ -112,23 +112,23 @@ survClass <- R6::R6Class(
             if ( is.null(self$options$groups))
                 group <- 1
             tmpDat$group <- group
-            
+
             ############################
             ### use only complete cases
             ############################
             tmpDat <- na.omit(tmpDat)
-            
-            
+
+
             ############################
             ### Check if the data.frame
             ### contains data at all
             ############################
             if (nrow(tmpDat) == 0)
                 stop('Data contains no (complete) rows')
-            
+
 
             ##################################
-            ### Populate first table with the 
+            ### Populate first table with the
             ### event summary
             ##################################
             table1 <- self$results$summary
@@ -149,7 +149,7 @@ survClass <- R6::R6Class(
                         exp=fit$exp[i]
                     ))
                     }
-                }            
+                }
 
 
             #######################################
@@ -159,7 +159,7 @@ survClass <- R6::R6Class(
             s <- survival::survfit(survival::Surv(times, status) ~ group, data=tmpDat)
             if (is.null(self$options$groups)) {
                 temp <- as.data.frame(rbind(summary(s)$table,summary(s)$table))[1,]
-            } 
+            }
             if (!is.null(self$options$groups)) temp <- as.data.frame(summary(s)$table)
             table3 <- self$results$medianestimates
             for (i in 1:length(unique(tmpDat$group)) ) {
@@ -169,17 +169,17 @@ survClass <- R6::R6Class(
                     ciub=temp$`0.95UCL`[i]
                 ))
             }
-            
-            
+
+
             #################################
             #### Set the state for the plots
             #################################
             self$results$sc$setState(tmpDat)
             self$results$chf$setState(tmpDat)
-            
-            
+
+
             #---------------------------------
-            #-- Populates the table with the  
+            #-- Populates the table with the
             #-- tests results
             #-- Only if there is a grouping
             #-- variable
@@ -207,38 +207,38 @@ survClass <- R6::R6Class(
                 # ------- add the df and the p-values
                 DF$df <- df
                 DF$pvalue <- pchisq(DF$chisqr, df, lower.tail = FALSE)
-                
-                
+
+
                 #################################
-                ### Populates the table with the  
+                ### Populates the table with the
                 ### tests results
                 #################################
                 tt <- self$results$tests
                 if (tt$isNotFilled() && length(self$options$tests) > 0) {
-                    
+
                     for (i in seq_along(self$options$tests)) {
-                        
+
                         test <- self$options$tests[i]
-                        
+
                         if (tt$isFilled(rowKey=1, col=paste0('chisqr[', test, ']')))
                             next()
-                        
+
                         private$.checkpoint()
                         row <- list()
                         row[[paste0('chisqr[', test, ']')]] <- DF$chisqr[i]
                         row[[paste0('df[', test, ']')]] <- DF$df[i]
                         row[[paste0('pvalue[', test, ']')]] <- DF$pvalue[i]
-                        
+
                         tt$setRow(rowKey=1, values=row)
-                        
+
                     }
 
                 }
 
             }
-            
+
             #---------------------------------
-            #-- Populates the table with the  
+            #-- Populates the table with the
             #-- tests results with multiple
             #-- comparisons.
             #-- Only if there is a grouping
@@ -252,10 +252,10 @@ survClass <- R6::R6Class(
                     return()
                 }
                 if (tt$isNotFilled() && self$options$testspw) {
-                    
+
                     groups <- private$.groups()
                     ngroups <- length(groups)
-                    
+
                     if (length(groups) > 2) {
                         groupsData <- list()
                         for (group in groups) {
@@ -263,54 +263,61 @@ survClass <- R6::R6Class(
                             groupsData[[group]] <- subset(tmpDat, subset=ss, select=c('times', 'status'))
                         }
                     }
-                    
+
                     for (pair in tt$rowKeys) {
-                        
+
                         x <- groupsData[[pair[1]]]
                         y <- groupsData[[pair[2]]]
-                        
+
                         for (i in seq_along(self$options$tests)) {
                             test <- self$options$tests[i]
-                            
+
                             if (tt$isFilled(rowKey=pair, col=paste0('nupw[', test, ']')))
                                 next()
-                            
+
                             private$.checkpoint()
-                            
+
+                            testArg <- test
+                            if (test == 'taroneware') {
+                                testArg <- 'tarone-ware'
+                            } else if (test == 'petopeto') {
+                                testArg <- 'peto-peto'
+                            }
+
                             result <- EnvStats::twoSampleLinearRankTestCensored(
-                                test = test,
+                                test = testArg,
                                 x = x$times,
                                 x.censored = ! x$status,
                                 y = y$times,
                                 y.censored = ! y$status,
                                 censoring.side = 'right')
-                            
+
                             row <- list()
-                            
+
                             row[[paste0('zpw[', test, ']')]] <- result$statistic['z']
                             row[[paste0('nupw[', test, ']')]] <- result$statistic['nu']
                             row[[paste0('nusepw[', test, ']')]] <- sqrt(result$statistic['var.nu'])
                             row[[paste0('ppw[', test, ']')]] <- result$p.value * min(ngroups*(ngroups - 1)/2,1)
-                            
+
                             tt$setRow(rowKey=pair, values=row)
                         }
-                       
-                        tt$setNote("note","P-values are Bonferroni-corrected.") 
+
+                        tt$setNote("note","P-values are Bonferroni-corrected.")
                     }
                 }
             }
 
         },
         .plot=function(image, theme, ggtheme, ...) {
-            
+
             eventVarName <- self$options$event
             elapsedVarName <- self$options$elapsed
-            
+
             if (is.null(eventVarName) || is.null(elapsedVarName))
                 return()
-            
-            tmpDat <- image$state 
-            
+
+            tmpDat <- image$state
+
             s <- survival::survfit(survival::Surv(times, status) ~ group, data=tmpDat)
             if (!is.null(names(s$strata))) names(s$strata) <- gsub("group=", "", names(s$strata))
 
@@ -322,13 +329,13 @@ survClass <- R6::R6Class(
             if (is.null(self$options$groups)) {
                 legend <-"none"
                 legendtitle <- NULL
-                } 
+                }
             xlab <- "Time"
             if (!self$options$units=="") xlab <- paste("Time (",self$options$units,")",sep="")
             survmedianline <-"none"
             if (self$options$median) survmedianline <- "hv"
-            
-            
+
+
             if (identical(image, self$results$sc)) {
                 func <- NULL
                 ylab <- "Survival Probabilities"
@@ -348,8 +355,8 @@ survClass <- R6::R6Class(
                     risk <- FALSE
                     survmedianline <-"none"
                     }
-                } 
-                    
+                }
+
             p <- survminer::ggsurvplot(s,
                                        data = tmpDat,
                                        xlab=xlab,
